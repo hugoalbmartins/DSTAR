787e2110b12d15a95f24784db98b565e911ec3c2
hugolyso-ai <hugolyso@gmail.com>
2025-12-29 09:40:17 +0000

    Updated vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,6 +9,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  define: {
+    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify('https://lunifcuuhhsacibvbvwi.supabase.co'),
+    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1bmlmY3V1aGhzYWNpYnZidndpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYzODY5OTksImV4cCI6MjA4MTk2Mjk5OX0.nFBKnJBqEARvErvygrNfHDY6KuAsgomSchhfB0L8iIE'),
+  },
   server: {
     port: 3000,
     host: true,
