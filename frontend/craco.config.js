68ce051d6cd416ebad21afda842f7647b4788c5b
hugolyso-ai <hugolyso@gmail.com>
2025-12-29 09:22:44 +0000

    Updated craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -5,5 +5,21 @@ module.exports = {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
+    configure: (webpackConfig) => {
+      webpackConfig.plugins = webpackConfig.plugins.filter(
+        plugin => plugin.constructor.name !== 'ESLintWebpackPlugin'
+      );
+      webpackConfig.ignoreWarnings = [/Failed to parse source map/];
+      return webpackConfig;
+    },
+  },
+  eslint: {
+    enable: false,
+    mode: "file",
+    configure: () => {
+      return {
+        rules: {},
+      };
+    },
   },
 };
